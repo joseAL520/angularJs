e5860d752c557efb9f769bcd418b507513335bd7
Jose Alvarez <jose.albelt1817@gmail.com>
2023-01-12 16:08:30 -0500

    data binding 2

--- data_Binding_controller/controller.js
+++ data_Binding_controller/controller.js
@@ -1,7 +1,19 @@
 
 angular.module("MyFistApp", [])
 .controller("FirstController", function($scope){ 
-
+    
     $scope.nombre = "jose";
+    $scope.nuevoUsuario = {};
+    $scope.usuarios=[
+        {comentario: "buen comentario", username: "joseAngel"},
+        {comentario: "mal comentario",  username: "otro usuario"}
+    ];
+
+
+    // funciones
+    $scope.agregarComentario = function(){
+      $scope.usuarios.push($scope.nuevoUsuario);   
+      $scope.nuevoComenatio={};
+    }
 });
 
