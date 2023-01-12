8d1f71d255451d830abc761e7681986be8ee52e1
Jose Alvarez <jose.albelt1817@gmail.com>
2023-01-12 16:41:24 -0500

    api-Rest

---
+++ 04api_Rest/api.js
@@ -0,0 +1,8 @@
+// usamos protocolos http  usamos una pagina https://jsonplaceholder.typicode.com para el api rest
+angular.module("MyFirstApp", [])
+    .controller("FistController", function($scope){
+
+
+    
+
+});
