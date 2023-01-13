9c8c9b820bb9e5debe9dcb0925e4a8f4eefc282c
Jose Alvarez <jose.albelt1817@gmail.com>
2023-01-13 08:33:07 -0500

    api-Rest get y post

--- 04api_Rest/api.js
+++ 04api_Rest/api.js
@@ -1,8 +1,29 @@
 // usamos protocolos http  usamos una pagina https://jsonplaceholder.typicode.com para el api rest
 angular.module("MyFirstApp", [])
-    .controller("FistController", function($scope){
+  .controller("FirstController", function($scope, $http){  
+    $scope.posts=[];
+    $scope.newUser = [];
+// get llamado de la lista  
+    $http.get("https://jsonplaceholder.typicode.com/posts/") 
+    .then(function(user){
+        console.log(user);  
+        $scope.posts = user.data;      
+    });
+
+// post agregar a la lista
+ $scope.addApiRest = function(){
+    $http.post("https://jsonplaceholder.typicode.com/posts/",{
+        title: $scope.newUser.title,
+        body: $scope.newUser.body,
+        userId:1})
+    .then(function(data,status,headers,config){
+        $scope.posts.push($scope.newUser);
+        $scope.newUser= {};
+
+    });
+ }
 
 
-    
+
 
 });
