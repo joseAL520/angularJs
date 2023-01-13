9eaddfd32b6626e0e15503fda15e5e3f9127f5d7
Jose Alvarez <jose.albelt1817@gmail.com>
2023-01-13 16:41:40 -0500

    factori new

--- 10factories/cotroller.js
+++ 10factories/cotroller.js
@@ -1,25 +1,74 @@
+// se reciclo parte de codigo del tema to do list
 
 angular.module("toDoList", ["LocalStorageModule"])
-  .controller("ToDoController", function($scope, localStorageService){
-   if(localStorageService.get("angular-todolist")){
-    $scope.todo= localStorageService.get("angular-todolist");
-   }else{
-    $scope.todo = [];
-   }
-    
-    /*
-        {
-            actividad: 'Terminar el curso Angular',
-            fecha:'3-03-15 2:00 pm'
-        }
-    */
-    $scope.$watchCollection('todo',function(newValue,oldValue){
-        localStorageService.set("angular-todolist",$scope.todo);
-    });
-    $scope.addActv = function(){
-        $scope.todo.push($scope.newActv);
-        $scope.newActv = {};
-        localStorageService.set($scope.todo);
+ .factory('ToDoService', function(localStorageService){
+    var toDoService = {};
+    toDoService.key = "angular-todolist";
+    if(localStorageService.get(toDoService.ke)){
+        toDoService.activities= localStorageService.get(toDoService.key);
+    }else{
+        toDoService.activities = [];
     }
+
+    // agregar
+    toDoService.add = function(newActv){
+        toDoService.activities.push(newActv);
+    }
+
+    //actiualizar
+    toDoService.updaLocalStorage = function(){
+        localStorageService.set(toDoService.key, toDoService.activities);
+    }
+
+    //limpiar
+    toDoService.clean = function(){
+        toDoService.activities = [];
+        toDoService.updaLocalStorage();
+        return toDoService.getAll();
+    } 
+
+    toDoService.getAll = function(){
+        return toDoService.activities;
+    }
+
+    // filtro
+    toDoService.removeItem = function(item){
+        toDoService.activities = toDoService.activities.filter(function(activty){
+            return activty !== item;
+        });
+        toDoService.updaLocalStorage();
+        return toDoService.getAll();
+    }
+
+   return toDoService;
+})
+
+.controller("ToDoController", function($scope, ToDoService){
+
+// los watch son una mala practica  porque son mas dificil de testear
+    /*
+         $scope.$watchCollection('todo',function(newValue,oldValue){
+        localStorageService.set("angular-todolist",$scope.todo);
     
+    });*/  
+    
+    $scope.todo = ToDoService.getAll();
+    $scope.newActv = {};
+
+    // agregar un elemento
+    $scope.addActv = function(){
+       ToDoService.add($scope.newActv);
+       $scope.newActv = {};
+    }
+
+    //remover un elemento
+    $scope.removeActv = function(item){
+       $scope.todo = ToDoService.removeItem(item);
+    }
+
+    // limpiar
+    $scope.cleanActvs = function(){
+        $scope.todo = ToDoService.clean();
+    }
+
 });
