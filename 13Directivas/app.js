7ec2fb989f51094919a8c5e193f5c0e3d4102378
Jose Alvarez <jose.albelt1817@gmail.com>
2023-01-15 13:11:02 -0500

    ngsrc and ngCloak

---
+++ 13Directivas/app.js
@@ -0,0 +1,24 @@
+angular.module("CustomDirective",[])
+.directive('backImg',function(){
+    return function(scope,elemnt,attrs){
+        attrs.$observe('backImg',function(value){
+            elemnt.css({
+                "background":"url('"+value+"')",
+                "background-size": "cover",
+                "background-position": "center" 
+            })
+        });
+    }
+
+})
+
+//style="background:url({{repo.owner.avatar_url}}); background-position: center; background-size: cover;">
+.controller("AppCtrl", function($scope,$http){
+    $scope.repos=[];
+    $http.get("https://api.github.com/users/joseAL520/repos") 
+    .then(function(respitorio){
+        console.log(respitorio);  
+        $scope.repos =respitorio.data;      
+    });
+
+});
