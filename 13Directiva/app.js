6778cce8ea7301c31c9ac039fa8d617d2b3e471a
Jose Alvarez <jose.albelt1817@gmail.com>
2023-01-15 13:19:35 -0500

    directivas

--- 13Directiva/app.js
+++ 13Directiva/app.js
@@ -1,9 +1,10 @@
 angular.module("CustomDirective",[])
 .directive('backImg',function(){
+
     return function(scope,elemnt,attrs){
         attrs.$observe('backImg',function(value){
             elemnt.css({
-                "background":"url('"+value+"')",
+                "background":"url("+value+")",
                 "background-size": "cover",
                 "background-position": "center" 
             })
